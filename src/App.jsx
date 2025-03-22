8057a92a4095f8ade2f5b293ab49fd5b5ba5960d
MTJonathan <jonathan02330@gmail.com>
2025-03-22 14:16:18 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Banner from './components/Banner/Banner'
+import './assets/style/App.css'
+
+function App() {
+
+  return (
+    <main>
+      <h1>Hello</h1>
+    </main>
+  )
+}
+
+export default App
